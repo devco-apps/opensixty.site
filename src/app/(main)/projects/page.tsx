7e8136b33c971ea8631a44d9a60134139fd2e587
bishhop5 <munyaradziranga@gmail.com>
2026-04-11 15:29:39 +0200

    feat: added projects page

--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -1,13 +1,21 @@
 import { Section } from "@/components/layout/Section";
 
+import WatermarkHeader from "@/components/typography/WatermarkHeader";
+import Gallery from "@/features/projects/Gallery";
+
 export default function ProjectsPage() {
   return (
     <div className="py-20">
-      <Section className="space-y-6">
-        <h1 className="text-4xl font-bold tracking-tight">Our Projects</h1>
-        <p className="text-lg text-gray-600">
-          Explore our portfolio of past work and initiatives.
-        </p>
+      <Section className="space-y-12">
+        <div className="text-center">
+          <WatermarkHeader text="Our Projects" backText="Here to Serve" />
+
+          <p className="text-lg text-gray-600">
+            Explore our portfolio of past work and initiatives.
+          </p>
+        </div>
+
+        <Gallery />
       </Section>
     </div>
   );
